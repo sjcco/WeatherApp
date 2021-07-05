587fa61262426e0e5290056ea3b3df6994431f5c
sjcco <73748496+sjcco@users.noreply.github.com>
2021-07-05 18:45:00 -0500

    Add api manager

---
+++ src/modules/weatherApi.js
@@ -0,0 +1,13 @@
+const getAdress = (location) => {
+  return `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${process.env.WEATHER_API_KEY}`;
+}
+
+async function getWeather(location) {
+    const response = await fetch(getAdress(location), {mode: 'cors'});
+    const weatherData = await response.json();
+    return weatherData;
+
+  
+}
+
+export { getWeather };
