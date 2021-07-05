96edada8560779619b3b193b427d189b5f41ef55
sjcco <73748496+sjcco@users.noreply.github.com>
2021-07-05 18:34:00 -0500

    Add event to read user input in search field

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
+import '@fortawesome/fontawesome-free/js/all';
 import './main.scss';
+import { getWeather } from './modules/weatherApi';
+import { errorModal, searchBar, searchBtn } from './modules/dom';
+import { fillPage } from './modules/helpers';
+
+
+searchBtn.addEventListener("click", (e) => {
+  e.preventDefault;
+  getWeather(searchBar.value.trim())
+  .then((response) => {
+    console.log(response.cod)
+    if(response.cod === '404') {
+      throw new Error('City not found');
+    } else {
+      console.log(response);
+      fillPage(response);
+    }
+  })
+  .catch((err) => {
+    console.log('catched');
+    console.log(err);
+    errorModal.show();
+  })
+});
